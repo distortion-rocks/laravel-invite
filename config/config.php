1885515cf4c5ccb33981b670bfb04820d36ade43
Vaugen Wakeling <vaugen.wakeling@sykescottages.co.uk>
2021-04-09 12:56:00 +0100

    Adding config options

--- config/config.php
+++ config/config.php
@@ -5,4 +5,20 @@
  */
 return [
 
+    /**
+     * Default expiration time period
+     * Time defined in hours
+     */
+    'expire' => 48,
+
+    /**
+     * User model
+     */
+    'Model' => App\Models\User::class,
+
+    /**
+     * Invitation model
+     */
+    'InviteModel' => Distortion\LaravelInvite\Models\InviteModel::class
+
 ];
---
+++ src/Models/InviteModel.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Distortion\LaravelInvite\Models;
+
+class InviteModel {}
